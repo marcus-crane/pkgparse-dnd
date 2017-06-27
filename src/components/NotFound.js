7663ddbe7a54326ee7385bdb1381a995c616c3e6
Marcus <marcus@thingsima.de>
2017-06-27 16:16:37 +1200

    Moved everything into client or server folders

--- src/components/NotFound.js
+++
@@ -1,8 +0,0 @@
-import React from 'react'
-import Layout from './Layout'
-
-const NotFound = () => {
-  return <Layout title="404 Not Found" subtitle="Uh oh, that's not a valid page!" />
-}
-
-export default NotFound
